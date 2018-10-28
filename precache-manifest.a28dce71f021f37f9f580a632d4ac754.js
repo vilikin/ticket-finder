460b8a41312bfed1cc89cf70f8c1277cd66654a0
Vili Kinnunen <vili.kinnunen1@gmail.com>
2018-10-28 12:16:42 +0200

    Updates

---
+++ precache-manifest.a28dce71f021f37f9f580a632d4ac754.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b64019877babf66d841b",
+    "url": "/ticket-finder/static/js/runtime~main.b6401987.js"
+  },
+  {
+    "revision": "54ffa92eb6f8d2c10bf1",
+    "url": "/ticket-finder/static/js/main.54ffa92e.chunk.js"
+  },
+  {
+    "revision": "59ab3a86ec7d2bd99b4a",
+    "url": "/ticket-finder/static/js/1.59ab3a86.chunk.js"
+  },
+  {
+    "revision": "54ffa92eb6f8d2c10bf1",
+    "url": "/ticket-finder/static/css/main.c2dd1aae.chunk.css"
+  },
+  {
+    "revision": "08f7ec7bf2e4a3bc38e64fb420c278ba",
+    "url": "/ticket-finder/index.html"
+  }
+];
